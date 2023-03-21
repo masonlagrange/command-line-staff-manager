3b56f51be1d7ff84b690f5048ca277cbd5650be5
MasonLagrange <mason.lagrange@gmail.com>
2023-03-20 23:28:07 -0400

    complete functionality

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
 const express = require('express')
 const mysql = require('mysql2')
+
+const PORT = 3001
+const app = express()
+
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'temp46Pass71',
+        database: 'staff_db'
+    }
+)
+
+app.use((req, res) => {
+    res.status(404).end()
+})
+
+app.listen(PORT)
+
+module.exports = connection
